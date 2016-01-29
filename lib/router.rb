003c767e23206d990f7f74232e715fd6a9305ee8
Ben Simpson <benfrasersimpson@gmail.com>
2016-01-29 09:41:38 +0100

    Add form view for index page

--- lib/router.rb
+++ lib/router.rb
@@ -1,13 +1,14 @@
 require 'sinatra/base'
 
 class FyberServer < Sinatra::Base
+  set :views, 'views'
 
   def self.run!
     super
   end
 
   get '/' do
-    return "Hello World"
+    erb :form
   end
 
 
