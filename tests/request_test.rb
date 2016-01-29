4ca44ff9294ca6c93bf4fb2efc4405b3383bf3c8
Ben Simpson <benfrasersimpson@gmail.com>
2016-01-29 09:01:17 +0100

    Add test for getting request_uri from request object

--- tests/request_test.rb
+++ tests/request_test.rb
@@ -52,4 +52,22 @@ class TestRequest < Minitest::Test
       @request.send(:param_string)
     }
   end
+
+  def test_request_url_created_correctly
+    expected_url = "http://api.fyber.com/feed/v1/offers.json?appid=157&device_id=2b6f0cc904d137be2e1730235f5664094b831186&ip=212.45.111.17&locale=de&page=2&ps_time=1312211903&pub0=campaign2&timestamp=1312553361&uid=player1&hashkey=7a2b1604c03d46eec1ecd4a686787b75dd693c4d"
+
+    @request[:appid] = 157
+    @request[:uid] = "player1"
+    @request[:ip] = "212.45.111.17"
+    @request[:locale] = "de"
+    @request[:device_id] = "2b6f0cc904d137be2e1730235f5664094b831186"
+    @request[:ps_time] = 1312211903
+    @request[:pub0] = "campaign2"
+    @request[:page] = 2
+
+    Request.api_key = "e95a21621a1865bcbae3bee89c4d4f84"
+    timestamp = 1312553361
+
+    assert_equal(expected_url, @request.request_url)
+  end
 end
